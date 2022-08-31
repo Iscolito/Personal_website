dd4aefda78eed1bddb2f9d1afe5590c2c8f75786
Iscolito <108865458+Iscolito@users.noreply.github.com>
2022-08-31 17:12:28 +0800

    Add files via upload

---
+++ operation_note1.py
@@ -0,0 +1,24 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/8/23 17:08
+# @Author : Iscolito
+# @File : algorithm_note1.py
+# @Software : PyCharm
+import sys
+from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+
+@app.route('/')
+def operation_note1():
+    return render_template('operation_note1.html')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        print("Building website...")
+        freezer.freeze()
+    else:
+        app.run(debug=True)
